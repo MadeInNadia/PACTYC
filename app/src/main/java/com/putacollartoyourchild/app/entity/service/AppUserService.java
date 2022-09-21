ed674402a68659927af072c35ffd0c9198f0cdcb
madeinadia <nadiazero7@gmail.com>
2022-09-21 16:19:21 +0100

    first commit

---
+++ app/src/main/java/com/putacollartoyourchild/app/entity/service/AppUserService.java
@@ -0,0 +1,29 @@
+package com.putacollartoyourchild.app.entity.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.putacollartoyourchild.app.entity.dao.IAppUserDao;
+import com.putacollartoyourchild.app.entity.model.AppUser;
+
+@Service
+public class AppUserService implements IAppUserService{
+	
+	@Autowired
+	IAppUserDao appUserDao;
+
+	@Override
+	public List<AppUser> getAll() {
+		return (List<AppUser>) appUserDao.findAll();
+		
+	}
+
+	@Override
+	public AppUser getOne(long id) {
+		return appUserDao.findById(id).get();
+	}
+
+}
---
+++ app/src/main/java/com/putacollartoyourchild/app/entity/service/IAppUserService.java
@@ -0,0 +1,13 @@
+package com.putacollartoyourchild.app.entity.service;
+
+import java.util.List;
+
+import com.putacollartoyourchild.app.entity.model.AppUser;
+
+public interface IAppUserService {
+	
+	public List<AppUser> getAll();
+	
+	public AppUser getOne(long Id);
+
+}
